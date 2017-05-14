df5b28e8e121091ba63eea6a576b0f06810673e3
Lev Levin <levlevin@RetinaBook.local>
2017-05-15 02:59:18 +0300

    Navbar 2.1
    Better navbar + hamburger menu for narrow displays

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,13 @@ window.onscroll = function () {
     "use strict";
     console.log(window.pageYOffset);
     var nav = document.getElementById('nav');
+    var labelid = document.getElementById('labelid');
     if (window.pageYOffset > 110) {
         nav.classList.add("scrolled");
+        labelid.classList.add("scrolled");
+
     } else {
         nav.classList.remove("scrolled");
+        labelid.classList.remove("scrolled");
     }
 };
