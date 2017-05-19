481254e87e57bca1604bd28c5eed715dc3e5e976
Lev Levin <levlevin@RetinaBook.local>
2017-05-19 18:08:31 +0300

    Major navigation news
    -Proper navbar (js)
    -Single .css

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,74 @@
 /*jslint devel: true*/
+function side() {
+    "use strict";
+    var nav = document.getElementById('nav');
+    var wrapper = document.getElementById('wrapper');
+    var button = document.getElementById('hamburger');
+    if (wrapper.classList.contains('moveRight')) {
+        wrapper.classList.remove("moveRight");
+        nav.classList.remove("moveRight");
+        button.classList.remove("moveRight");
+    } else {
+        wrapper.classList.add("moveRight");
+        nav.classList.add("moveRight");
+        button.classList.add("moveRight");
+        nav.classList.remove("scrolled");
+
+    }
+    return;
+}
 
 window.onscroll = function () {
     "use strict";
-    console.log(window.pageYOffset);
     var nav = document.getElementById('nav');
-    var labelid = document.getElementById('labelid');
-    if (window.pageYOffset > 110) {
+    if (document.documentElement.clientWidth > 800 && window.pageYOffset > 110) {
         nav.classList.add("scrolled");
-        labelid.classList.add("scrolled");
-
     } else {
         nav.classList.remove("scrolled");
-        labelid.classList.remove("scrolled");
     }
-};
+
+}
+
+window.onresize = function () {
+    "use strict";
+    var nav = document.getElementById('nav');
+    var wrapper = document.getElementById('wrapper');
+    var button = document.getElementById('hamburger');
+
+    if (document.documentElement.clientWidth > 800 && window.pageYOffset > 110) {
+        nav.classList.add("scrolled");
+        wrapper.classList.remove("moveRight");
+        nav.classList.remove("moveRight");
+        button.classList.remove("moveRight");
+    } else if (document.documentElement.clientWidth > 800 && window.pageYOffset <= 110) {
+        nav.classList.remove("scrolled");
+        wrapper.classList.remove("moveRight");
+        nav.classList.remove("moveRight");
+        button.classList.remove("moveRight")
+    } else {
+        nav.classList.remove("scrolled");
+    }
+
+}
+
+window.onload = function () {
+    "use strict";
+    var nav = document.getElementById('nav');
+    var wrapper = document.getElementById('wrapper');
+    var button = document.getElementById('hamburger');
+
+    if (document.documentElement.clientWidth > 800 && window.pageYOffset > 110) {
+        nav.classList.add("scrolled");
+        wrapper.classList.remove("moveRight");
+        nav.classList.remove("moveRight");
+        button.classList.remove("moveRight");
+    } else if (document.documentElement.clientWidth > 800 && window.pageYOffset <= 110) {
+        nav.classList.remove("scrolled");
+        wrapper.classList.remove("moveRight");
+        nav.classList.remove("moveRight");
+        button.classList.remove("moveRight")
+    } else {
+        nav.classList.remove("scrolled");
+    }
+
+}
