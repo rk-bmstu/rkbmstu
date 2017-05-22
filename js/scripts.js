091307ae1d2030134fcbf86ee8d07b11c14b771b
Lev Levin <levlevin@RetinaBook.local>
2017-05-22 04:32:18 +0300

    Work in progress
    -Better js
    -Better hamburger menu
    -Minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,124 @@
-/*eslint-env browser*/ /*exported side*/
+/*eslint-env browser*/ /*exported active*/
 
-// Making hamburger menu work by adding "moveRight" class //
+"use strict";
 
-function side() {
-  "use strict";
-  var nav = document.getElementById('nav'),
-    wrapper = document.getElementById('wrapper'),
-    hamburger = document.getElementById('hamburger');
-  if (wrapper.classList.contains('moveRight')) {
-    wrapper.classList.remove("moveRight");
-    nav.classList.remove("moveRight");
-    hamburger.classList.remove("moveRight");
-  } else {
-    wrapper.classList.add("moveRight");
-    nav.classList.add("moveRight");
-    hamburger.classList.add("moveRight");
-    nav.classList.remove("scrolled");
-  }
-  return;
+
+// Highlighting current link in the navbar by adding "current" class //
+
+function current() {
+	if (document.documentElement.clientWidth >= 800) {
+		var navLinks = document.querySelectorAll('#nav a'); // All links inside the nav
+
+		for (var i = 0; i < navLinks.length; i++) {
+			var link = navLinks[i],
+				url = link.getAttribute('href');
+			if (window.location.href == url) {
+				link.classList.add('current');
+			}
+		}
+	}
+}
+
+
+// Fixing overflow //
+
+function fixScroll() {
+	var i = 0,
+		body = document.getElementsByTagName('body');
+
+	for (i = 0; i < body.length; i++) {
+		body[i].classList.toggle('noScroll');
+	}
+
+	document.getElementById('nav').classList.toggle('yeahScroll');
+}
+
+
+// Removing overflow //
+
+function removeScroll() {
+	var i = 0,
+		body = document.getElementsByTagName('body');
+
+	for (i = 0; i < body.length; i++) {
+		body[i].classList.remove('noScroll');
+	}
+
+	document.getElementById('nav').classList.remove('yeahScroll');
+}
+
+
+// Making hamburger menu work by adding "active" modifier class //
+
+function active() {
+	var i = 0,
+		logo = document.getElementsByClassName('logo'),
+		menu = document.getElementsByClassName('menu');
+
+	document.getElementById('hamburger').classList.toggle('active');
+	document.getElementById('nav').classList.toggle('active');
+
+	for (i = 0; i < logo.length; i++) {
+		logo[i].classList.toggle('active');
+	}
+
+	for (i = 0; i < menu.length; i++) {
+		menu[i].classList.toggle('active');
+	}
+
+	fixScroll();
+}
+
+
+// Removing "active" modifier class from all elements //
+
+function removeActive() {
+	var i = 0,
+		everyActive = document.getElementsByClassName('active');
+
+	for (i = 0; i < everyActive.length; i++) {
+		everyActive[i].classList.remove('active');
+	}
+	removeScroll();
 }
 
 // Making navbar change on scroll and resize //
 
 function iFixit() {
-  "use strict";
-  var nav = document.getElementById('nav'),
-    wrapper = document.getElementById('wrapper'),
-    hamburger = document.getElementById('hamburger');
+	var nav = document.getElementById('nav');
 
-  if (document.documentElement.clientWidth > 800 && window.pageYOffset > 110) {
-    nav.classList.add("scrolled");
-    wrapper.classList.remove("moveRight");
-    nav.classList.remove("moveRight");
-    hamburger.classList.remove("moveRight");
-  } else if (document.documentElement.clientWidth > 800 && window.pageYOffset <= 110) {
-    nav.classList.remove("scrolled");
-    wrapper.classList.remove("moveRight");
-    nav.classList.remove("moveRight");
-    hamburger.classList.remove("moveRight");
-  } else {
-    nav.classList.remove("scrolled");
-  }
+	if (document.documentElement.clientWidth >= 800 && window.pageYOffset > 110) {
+		nav.classList.add("scrolled");
+		removeActive();
+	} else
+	if (document.documentElement.clientWidth >= 800 && window.pageYOffset <= 110) {
+		nav.classList.remove("scrolled");
+		removeActive();
+	} else {
+		nav.classList.remove("scrolled");
+	}
 }
 
-window.onscroll = window.onresize = window.onload = iFixit;
 
 // Setting top margin of title to half of its height, setting bottom padding of previous parent //
 
 function marginTop() {
-  "use strict";
-  var title = document.getElementsByClassName('title');
-  for (var i = 0; i < title.length; i += 1) {
-    title[i].style.marginTop = -(title[i].clientHeight / 2) + "px";
-  }
-  for (i = 0; i < title.length - 1; i += 1) {
-    title[i].parentElement.style.paddingBottom = (title[i + 1].clientHeight - 40) + "px";
-  }
+	var title = document.getElementsByClassName('title');
+	for (var i = 0; i < title.length; i++) {
+		title[i].style.marginTop = -(title[i].clientHeight / 2) + "px";
+	}
+	for (i = 0; i < title.length - 1; i++) {
+		title[i].parentElement.style.paddingBottom = (title[i + 1].clientHeight - 40) + "px";
+	}
 }
 
-window.onload = marginTop;
+
+// Load functions on scroll, resize and load //
+
+function autoLoad() {
+	marginTop();
+	current();
+	iFixit();
+}
+
+window.onscroll = window.onresize = window.onload = autoLoad;
